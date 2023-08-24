f9a26791d2fe53eb98f2dba6582be05f1da29dd4
Kai <kmichaelmikhail@gmail.com>
2023-08-24 12:06:52 +0200

    fix heroku host url

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kais-django-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['kais-django-blog-7e7847d3c319.herokuapp.com', 'localhost']
 
 
 # Application definition
